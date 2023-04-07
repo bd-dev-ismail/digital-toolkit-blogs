4d1bfb77e3a5250c1529bd4bb1bade6a008ac54d
bd-dev-ismail <ismailratul7@gmail.com>
2023-04-08 05:51:13 +0600

    Good Morning

--- src/components/Dashboard/AddBlog/AddBlog.js
+++ src/components/Dashboard/AddBlog/AddBlog.js
@@ -9,13 +9,14 @@ const AddBlog = () => {
   const {
     register,
     handleSubmit,
-
     formState: { errors },
   } = useForm();
-  const handleAddBlog = (data) => {};
+  const handleAddBlog = (data) => {
+    console.log(data);
+  };
   return (
     <div>
-      <section className="max-w-4xl lg:w-[80rem] p-6 mx-auto bg-purple-400 rounded-md shadow-md dark:bg-gray-800 mt-5">
+      <section className="max-w-4xl lg:w-[80rem]  p-6 mx-auto bg-purple-400 rounded-md shadow-md dark:bg-gray-800 my-10 lg:mt-[5rem]">
         <h1 className="text-xl font-bold text-white capitalize dark:text-white">
           Add Blogs
         </h1>
@@ -24,19 +25,19 @@ const AddBlog = () => {
             <div>
               <label
                 className="text-white dark:text-gray-200"
-                htmlFor="username"
+                htmlFor="blogTitle"
               >
-                Product Name
+                Blog Title
               </label>
               <input
-                {...register("productName", { required: true })}
-                id="username"
+                {...register("blogTitle", { required: true })}
+                id="blogTitle"
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
-              {errors.productName && (
+              {errors.blogTitle && (
                 <span className="mt-3 text-gray-800">
-                  productName field is required
+                  blogTitle field is required
                 </span>
               )}
             </div>
@@ -44,188 +45,39 @@ const AddBlog = () => {
             <div>
               <label
                 className="text-white dark:text-gray-200"
-                htmlFor="productPrice"
+                htmlFor="blogBanner"
               >
-                Originial Price
+                Blog Banner URL
               </label>
               <input
-                {...register("originalPrice", { required: true })}
-                id="originalPrice"
-                type="number"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-              {errors.originalPrice && (
-                <span className="mt-3 text-gray-800">
-                  originalPrice field is required
-                </span>
-              )}
-            </div>
-            <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="resalePrice"
-              >
-                Resale Price
-              </label>
-              <input
-                {...register("resalePrice", { required: true })}
-                id="resalePrice"
-                type="number"
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-              {errors.resalePrice && (
-                <span className="mt-3 text-gray-800">
-                  resalePrice field is required
-                </span>
-              )}
-            </div>
-            <div>
-              <label className="text-white dark:text-gray-200" htmlFor="email">
-                Seller Email
-              </label>
-              <input
-                {...register("sellerEmail", { required: true })}
-                id="email"
-                type="email"
-                disabled
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              />
-              {errors.sellerEmail && (
-                <span className="mt-3 text-gray-800">
-                  sellerEmail field is required
-                </span>
-              )}
-            </div>
-            <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="sellerName"
-              >
-                Seller Name
-              </label>
-              <input
-                {...register("sellerName", { required: true })}
-                id="sellerName"
-                disabled
+                {...register("blogBanner", { required: true })}
+                id="blogBanner"
                 type="text"
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               />
+              {errors.blogBanner && (
+                <span className="mt-3 text-gray-800">
+                  blogBanner field is required
+                </span>
+              )}
             </div>
+
             <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="passwordConfirmation"
-              >
-                Location
+              <label className="text-white dark:text-gray-200" htmlFor="tag">
+                Blog Category
               </label>
+
               <select
-                {...register("location", { required: true })}
+                id="tag"
+                {...register("tag", { required: true })}
                 className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
               >
-                <option value="Chattogram">Chattogram</option>
-                <option value="Dhaka">Dhaka</option>
-                <option value="Kulna">Kulna</option>
-                <option value="Syhlet">Syhlet</option>
-                <option value="Barishal">Barishal</option>
-                <option value="Noakhali">Noakhali</option>
-                <option value="Other">Other</option>
+                <option value="">Select Categories</option>
+                <option value="Web Design">Web Design</option>
+                <option value="Javascript">Javascript</option>
+                <option value="CSS">CSS</option>
+                <option value="Jquery">Jquery</option>
               </select>
-            </div>
-            {errors.location && (
-              <span className="mt-3 text-gray-800">
-                location field is required
-              </span>
-            )}
-            <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="productConditon"
-              >
-                Product Condition
-              </label>
-              <select
-                {...register("productConditon", { required: true })}
-                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              >
-                <option value="Excellent">Excellent</option>
-                <option value="Good">Good</option>
-                <option value="Fair">Fair</option>
-              </select>
-              {errors.productConditon && (
-                <span className="mt-3 text-gray-800">
-                  productConditon field is required
-                </span>
-              )}
-            </div>
-            <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="productCategory"
-              >
-                Product Category
-              </label>
-              {loading ? (
-                <Loader />
-              ) : (
-                <>
-                  <select
-                    {...register("productCategoryId", { required: true })}
-                    className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-                  >
-                    {/* {categories.map((category) => (
-                      <option key={category._id} value={category?._id}>
-                        {category?.name}
-                      </option>
-                    ))} */}
-                  </select>
-                </>
-              )}
-            </div>
-            <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="yearOfPurchase"
-              >
-                Year Of Purchase
-              </label>
-              <select
-                {...register("yearOfPurchase", { required: true })}
-                className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              >
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
-              </select>
-              {errors.yearOfPurchase && (
-                <span className="mt-3 text-gray-800">
-                  yearOfPurchase field is required
-                </span>
-              )}
-            </div>
-            <div>
-              <label
-                className="text-white dark:text-gray-200"
-                htmlFor="yearOfPurchase"
-              >
-                Year Of Use
-              </label>
-              <select
-                {...register("yearOfUse", { required: true })}
-                className="block w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-              >
-                <option value="2018">2018</option>
-                <option value="2019">2019</option>
-                <option value="2020">2020</option>
-                <option value="2021">2021</option>
-                <option value="2022">2022</option>
-              </select>
-              {errors.yearOfUse && (
-                <span className="mt-3 text-gray-800">
-                  yearOfUse field is required
-                </span>
-              )}
             </div>
 
             <div>
@@ -241,40 +93,59 @@ const AddBlog = () => {
                 {/* <FaCalendarAlt className="absolute top-3 right-5" /> */}
               </div>
             </div>
-          </div>
-          <div className="my-3">
-            <label className="block text-sm font-medium text-white">
-              Product Image
-            </label>
-            <div className="space-y-1 text-center">
-              <div>
-                <input
-                  {...register("productImage", { required: true })}
-                  id="proudctImage"
-                  type="file"
-                  className="mt-1 text-white flex justify-center px-4 py-2 w-full border-2 border-gray-300 border-dashed rounded-md"
-                />
-                {errors.productImage && (
-                  <span className="mt-3 text-gray-800">
-                    productImage field is required
-                  </span>
-                )}
-              </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="descTitle"
+              >
+                Description Title
+              </label>
+              <input
+                {...register("descTitle", { required: true })}
+                id="descTitle"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+              {errors.descTitle && (
+                <span className="mt-3 text-gray-800">
+                  descTitle field is required
+                </span>
+              )}
+            </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="descImage"
+              >
+                Description Image
+              </label>
+              <input
+                {...register("descImage", { required: true })}
+                id="descImage"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+              {errors.descImage && (
+                <span className="mt-3 text-gray-800">
+                  descImage field is required
+                </span>
+              )}
             </div>
           </div>
+
           <div>
-            <label className="text-white dark:text-gray-200" htmlFor="desc">
-              Product Description
+            <label className="text-white dark:text-gray-200" htmlFor="blogDesc">
+              Blog Description
             </label>
             <textarea
-              {...register("productDesc", { required: true })}
-              id="desc"
+              {...register("blogDesc", { required: true })}
+              id="blogDesc"
               type="textarea"
               className="block h-36 w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
             ></textarea>
-            {errors.productDesc && (
+            {errors.blogDesc && (
               <span className="mt-3 text-gray-800">
-                productDesc field is required
+                blogDesc field is required
               </span>
             )}
           </div>
@@ -287,7 +158,7 @@ const AddBlog = () => {
             ) : (
               <input
                 type="submit"
-                value="Publish Product"
+                value="Publish Blog"
                 className="btn btn-outline text-white"
               />
             )}
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,22 +6,22 @@ export const Navbar = () => {
   const menu = (
     <>
       <li>
-        <Link to="/" class="text-white font-medium">
+        <Link to="/" className="text-white font-medium">
           Home
         </Link>
       </li>
       <li>
-        <Link to="/all-blogs" class="text-white font-medium">
+        <Link to="/all-blogs" className="text-white font-medium">
           All Blogs
         </Link>
       </li>
       <li>
-        <Link to="/history" class="text-white font-medium">
+        <Link to="/history" className="text-white font-medium">
           Reading History
         </Link>
       </li>
       <li>
-        <Link to="/dashboard" class="text-white font-medium">
+        <Link to="/dashboard" className="text-white font-medium">
           Dashboard
         </Link>
       </li>
@@ -29,11 +29,11 @@ export const Navbar = () => {
   );
   return (
     <div className=" bg-purple-400 ">
-      <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-        <div class="relative flex items-center justify-between">
-          <Link to="/" class="inline-flex items-center ">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <Link to="/" className="inline-flex items-center ">
             <svg
-              class="w-8 text-white "
+              className="w-8 text-white "
               viewBox="0 0 24 24"
               strokeLinejoin="round"
               strokeWidth="2"
@@ -47,19 +47,19 @@ export const Navbar = () => {
               <rect x="14" y="1" width="7" height="6" />
               <rect x="14" y="11" width="7" height="12" />
             </svg>
-            <span class="ml-2 text-xl  font-bold tracking-wide text-white uppercase">
+            <span className="ml-2 text-xl  font-bold tracking-wide text-white uppercase">
               Digital Toolkit Blogs
             </span>
           </Link>
-          <ul class="flex items-center hidden space-x-8 lg:flex">{menu}</ul>
-          <div class="lg:hidden">
+          <ul className="flex items-center hidden space-x-8 lg:flex">{menu}</ul>
+          <div className="lg:hidden">
             <button
               aria-label="Open Menu"
               title="Open Menu"
-              class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline text-white"
+              className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline text-white"
               onClick={() => setIsMenuOpen(true)}
             >
-              <svg class="w-5 text-white" viewBox="0 0 24 24">
+              <svg className="w-5 text-white" viewBox="0 0 24 24">
                 <path
                   fill="currentColor"
                   d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
@@ -75,13 +75,13 @@ export const Navbar = () => {
               </svg>
             </button>
             {isMenuOpen && (
-              <div class="absolute top-0 left-0 w-full">
-                <div class="p-5 bg-purple-400 border rounded shadow-sm">
-                  <div class="flex items-center justify-between mb-4">
+              <div className="absolute top-0 left-0 w-full">
+                <div className="p-5 bg-purple-400 border rounded shadow-sm">
+                  <div className="flex items-center justify-between mb-4">
                     <div>
-                      <Link to="/" class="inline-flex items-center">
+                      <Link to="/" className="inline-flex items-center">
                         <svg
-                          class="w-8 text-white"
+                          className="w-8 text-white"
                           viewBox="0 0 24 24"
                           strokeLinejoin="round"
                           strokeWidth="2"
@@ -95,7 +95,7 @@ export const Navbar = () => {
                           <rect x="14" y="1" width="7" height="6" />
                           <rect x="14" y="11" width="7" height="12" />
                         </svg>
-                        <span class="ml-2 text-xl font-bold tracking-wide text-white uppercase">
+                        <span className="ml-2 text-xl font-bold tracking-wide text-white uppercase">
                           Digital Toolkit Blogs
                         </span>
                       </Link>
@@ -104,10 +104,10 @@ export const Navbar = () => {
                       <button
                         aria-label="Close Menu"
                         title="Close Menu"
-                        class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+                        className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                         onClick={() => setIsMenuOpen(false)}
                       >
-                        <svg class="w-6 text-white" viewBox="0 0 24 24">
+                        <svg className="w-6 text-white" viewBox="0 0 24 24">
                           <path
                             fill="currentColor"
                             d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
@@ -117,7 +117,7 @@ export const Navbar = () => {
                     </div>
                   </div>
                   <nav>
-                    <ul class="space-y-4">{menu}</ul>
+                    <ul className="space-y-4">{menu}</ul>
                   </nav>
                 </div>
               </div>
